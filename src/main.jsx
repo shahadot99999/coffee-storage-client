206bd1142615ca6dec11ee868c89911461a82244
shahadot99999 <shahadot99999@gmail.com>
2025-09-24 19:18:38 +0600

    Client side Use SweetAlert set up

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        loader:()=>fetch('http://localhost:3000/coffees'),
         Component: Home
       }, 
       {
