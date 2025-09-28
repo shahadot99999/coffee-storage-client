f29731bbbc8d7bd301fcbe8c6affb31bdfac45f1
shahadot99999 <shahadot99999@gmail.com>
2025-09-28 19:24:17 +0600

    client side Delete user and recap signin user

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const SignIn = () => {
+
+    const handleSingIn = e =>{
+        e.preventDefault();
+    }
     return (
-        <div>
-            
+        <div className="card bg-base-100 mt-8  max-w-sm mx-auto shrink-0 shadow-2xl">
+            <div className="card-body">
+                <h1 className="text-5xl font-bold">Sign In now!</h1>
+                <form onSubmit={handleSingIn} className="fieldset">
+                        <label className="label">Email</label>
+                    <input type="email" name='email' className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" name='password' className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Sign In</button>
+                </form>
+            </div>
         </div>
     );
 };
