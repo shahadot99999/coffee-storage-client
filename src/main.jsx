37e4dd135ad5f7f1d9cfeecd7f369839976705b4
shahadot99999 <shahadot99999@gmail.com>
2025-09-28 17:41:31 +0600

    client side  display users in a table and create get

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import CoffeeDetails from './components/CoffeeDetails.jsx';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
 import AuthProvider from './contexts/AuthProvider.jsx';
+import Users from './components/Users.jsx';
 
 
 
@@ -48,6 +49,11 @@ const router = createBrowserRouter([
       {
         path: 'signup',
         Component: SignUp
+      },
+      {
+        path: 'users',
+        loader: ()=>fetch('http://localhost:3000/users'),
+        Component: Users
       },
     ]
   },
