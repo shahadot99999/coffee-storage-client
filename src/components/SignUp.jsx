24ff9246299e994f7190f18310fc2f7eeecc77b3
shahadot99999 <shahadot99999@gmail.com>
2025-09-28 16:03:21 +0600

    Client side post method CRUD Review apply

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
-import React, { use } from 'react';
+import React, {  use } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import Swal from 'sweetalert2';
 
 const SignUp = () => {
     const {createUser} = use(AuthContext);
@@ -9,14 +10,39 @@ const SignUp = () => {
              e.preventDefault(); 
             const form = e.target;
             const formData = new FormData(form);
-            const email = formData.get('email');
-            const password = formData.get('password');
-            console.log(email, password);
+
+            const {email, password, ...userProfile}= Object.fromEntries(formData.entries());
+
+            // const email = formData.get('email');
+            // const password = formData.get('password');
+            console.log(email, password, userProfile);
 
             //create user in the firebase
             createUser(email, password)
             .then(result =>{
                 console.log(result.user);
+
+                //save profile info in the db
+                fetch('http://localhost:3000/users', {
+                  method: 'POST'  ,
+                  headers:{
+                    'content-type': 'application/json'
+                  },
+                  body: JSON.stringify(userProfile)
+                })
+                .then(res =>res.json())
+                .then(data =>{
+                    // console.log("After Profile save", data);
+                    if(data.insertedId){
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: "Your account is created",
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                })
             })
             .catch((error) => {
                 console.log(error);
@@ -29,7 +55,15 @@ const SignUp = () => {
             <div className="card-body">
                 <h1 className="text-5xl font-bold">Sign Up now!</h1>
                 <form onSubmit={handleSingUp} className="fieldset">
-                    <label className="label">Email</label>
+                    <label className="label">Name</label>
+                    <input type="text" name='name' className="input" placeholder="Name" />
+                     <label className="label">Adress</label>
+                    <input type="text" name='adress' className="input" placeholder="Adress" />
+                     <label className="label">Phone</label>
+                    <input type="text" name='phone' className="input" placeholder="Phone" />
+                     <label className="label">Photo</label>
+                       <input type="text" name='photo' className="input" placeholder="Photo" />
+                        <label className="label">Email</label>
                     <input type="email" name='email' className="input" placeholder="Email" />
                     <label className="label">Password</label>
                     <input type="password" name='password' className="input" placeholder="Password" />
