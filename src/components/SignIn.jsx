730c245dd57de3e985633b5bf3e77ba130715a3d
shahadot99999 <shahadot99999@gmail.com>
2025-10-02 11:07:41 +0600

    update user login info after successfull login

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 const SignIn = () => {
 
+    const {signInUser}= use(AuthContext);
+
     const handleSingIn = e =>{
         e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+
+        //firebase sign in send
+        signInUser(email, password)
+        .then(result =>{
+            console.log(result.user);
+
+            const signInInfo = {
+                email,
+                lastSignInTime : result.user?.metadata?.lastSignInTime
+            }
+
+            //update last sign in to the database
+            fetch('http://localhost:3000/users', {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body:JSON.stringify(signInInfo)
+            })
+            .then(res=>res.json())
+            .then(data =>{
+                console.log('after update patch', data);
+            })
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
     return (
         <div className="card bg-base-100 mt-8  max-w-sm mx-auto shrink-0 shadow-2xl">
